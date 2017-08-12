47d08ab0265451bd8ecc00793ffa344f58474697
Hector Benitez <hector.benitez.fletes@gmail.com>
2017-08-12 12:26:08 -0500

    New Formats

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(function (angular) {
+    angular.module('TunaTechApp', [])
+        .controller('HomeController', HomeController)
+        .directive('lightboxDirective', lightboxDirective);
+
+    HomeController.$inject = ['$http'];
+
+    function HomeController($http) {
+        var vm = this;
+
+        vm.showSpeakerLightBox = false;
+
+        vm.activateSpeakerLightBox = activateSpeakerLightBox;
+
+        _init();
+
+        function _init() {
+            $http.get('./data/speakers.json')
+                .then(function (res) {
+                    vm.speakers = res.data;
+                });
+
+            $http.get('./data/schedule.json')
+                .then(function (res) {
+                    vm.schedule = res.data;
+                });
+        }
+
+        function activateSpeakerLightBox(speaker) {
+            vm.currentSpeaker = speaker;
+            vm.showSpeakerLightBox = true;
+        }
+    }
+
+    function lightboxDirective() {
+        return {
+            restrict: 'E', // applied on 'element'
+            transclude: true, // re-use the inner HTML of the directive
+            template: '<section ng-transclude></section>', // need this so that inner HTML will be used
+        }
+    }
+
+})(angular);
